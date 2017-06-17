1da025ca255994bc24a5eade68281687ca3b089b
Konstantin Luebeck <konstantin.luebeck@student.uni-tuebingen.de>
2017-06-17 16:32:41 +0200

    Introduced DEBUG makros to train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -74,7 +74,9 @@ static void train_lenet(const std::string &data_dir_path,
 
   construct_net(nn, backend_type);
 
+  #ifdef DEBUG
   std::cout << "load models..." << std::endl;
+  #endif
 
   // load MNIST dataset
   std::vector<label_t> train_labels, test_labels;
@@ -87,10 +89,12 @@ static void train_lenet(const std::string &data_dir_path,
   parse_mnist_images(data_dir_path + "/t10k-images.idx3-ubyte", &test_images,
                      -1.0, 1.0, 2, 2);
 
+  #ifdef DEBUG
   std::cout << "start training" << std::endl;
 
   progress_display disp(train_images.size());
   timer t;
+  #endif
 
   optimizer.alpha *=
     std::min(tiny_dnn::float_t(4),
@@ -99,23 +103,33 @@ static void train_lenet(const std::string &data_dir_path,
   int epoch = 1;
   // create callback
   auto on_enumerate_epoch = [&]() {
+
+    #ifdef DEBUG
     std::cout << "Epoch " << epoch << "/" << n_train_epochs << " finished. "
               << t.elapsed() << "s elapsed." << std::endl;
     ++epoch;
+
     tiny_dnn::result res = nn.test(test_images, test_labels);
     std::cout << res.num_success << "/" << res.num_total << std::endl;
 
     disp.restart(train_images.size());
     t.restart();
+    #endif
   };
 
-  auto on_enumerate_minibatch = [&]() { disp += n_minibatch; };
+  auto on_enumerate_minibatch = [&]() { 
+    #ifdef DEBUG 
+    disp += n_minibatch; 
+    #endif
+  };
 
   // training
   nn.train<mse>(optimizer, train_images, train_labels, n_minibatch,
                 n_train_epochs, on_enumerate_minibatch, on_enumerate_epoch);
 
+  #ifdef DEBUG
   std::cout << "end training." << std::endl;
+  #endif
 
   // test and show results
   nn.test(test_images, test_labels).print_detail(std::cout);
